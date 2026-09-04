7834f2c34ad7cf5681af52f66d0bf0d2431f8b2b
brokenlander <94259413+brokenlander@users.noreply.github.com>
2026-09-04 05:58:08 -0400

    Current agent: lighter lavender, no bold

--- src/render.c
+++ src/render.c
@@ -80,7 +80,7 @@ static void lb_pad_to(linebuf *lb, int col)
 #define C_DIM "\033[38;2;86;95;137m"
 #define C_HEAD "\033[38;2;122;162;247m"
 #define C_NAME "\033[38;2;145;180;250m" /* running agents */
-#define C_SELF "\033[1m\033[38;2;192;202;245m" /* the agent of this session: bold, near-white lavender */
+#define C_SELF "\033[38;2;213;220;255m" /* the agent of this session: a lighter lavender */
 
 static const char *status_colour(agent_status s);
 
